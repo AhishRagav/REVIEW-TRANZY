f83fe4222db908a12ab6345779dc2e69d108c5cb
AhishRagav <ahishragav16@gmail.com>
2025-05-28 14:15:38 +0530

    reviewcalculator created

---
+++ review/src/main/java/com/example/review/controller/Reviewcontroller.java
@@ -0,0 +1,27 @@
+package com.example.review.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.review.model.Review;
+import com.example.review.service.ReviewService;
+
+@RestController
+public class Reviewcontroller {
+    @Autowired
+    public ReviewService reviewService;
+
+    @PostMapping("/postreview")
+    public void postreview(@RequestBody Review review){
+        reviewService.saveReview(review);
+    }
+
+    @GetMapping("/getavg")
+    public float getrating(){
+        return reviewService.getrating();
+    }
+
+}   
---
+++ review/src/main/java/com/example/review/service/ReviewService.java
@@ -0,0 +1,31 @@
+package com.example.review.service;
+
+import com.example.review.model.Review;
+import com.example.review.repository.Repo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private Repo repo;
+
+    public void saveReview(Review review) {
+        repo.save(review);
+    }
+
+    public float getrating(){
+
+        List <Review> list=repo.findAll();
+        float sum=0;
+        for(int i=0;i<list.size();i++){
+            sum+=list.get(i).getReview();
+        }
+
+        return sum/(list.size());
+    }
+}
